0e894e38b4ad92cc813d4adafc213499d4ee334f
ywisax <25803471@qq.com>
2015-09-05 01:03:55 +0800

    增加配置文件自动加载

--- bootstrap-dev.php
+++ bootstrap-dev.php
@@ -1,6 +1,11 @@
 <?php
 
+use tourze\Base\Config;
+
 if (is_file(__DIR__ . '/vendor/autoload.php'))
 {
     require_once __DIR__ . '/vendor/autoload.php';
 }
+
+// 指定配置加载目录
+Config::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
